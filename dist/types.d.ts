ccaeb7a4f2680c7ca9ad3af2e7651ab677595221
Beorn Bishop <beorn@morpher.com>
2025-07-24 16:17:43 +0200

    Market Types

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -3,7 +3,7 @@ export type TExchangeRate = {
     eth_price: any;
     usdc_price: any;
 } | undefined;
-export type TMarketType = "stock" | "forex" | "commodity" | "index";
+export type TMarketType =  "stock" | "forex" | "commodity" | "index"  | "crypto" | "prediction" | "f1" | "mlb" | "unique";
 export type TCurrency = 'MPH' | 'USDC' | 'ETH';
 export type TAddress = `0x${string}`;
 export type TCurrencyDetails = {
